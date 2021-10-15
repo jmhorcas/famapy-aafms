34be328cdcc75c137dadd98f796157c93ff599e8
José Miguel Horcas <jhorcas@us.es>
2021-10-15 12:17:29 +0200

    Complete conversion to CNF

--- famapy/core/models/ast.py
+++ famapy/core/models/ast.py
@@ -74,6 +74,13 @@ class AST:
 
     def to_cnf(self) -> 'AST':
         return convert_into_cnf(self)
+
+    def get_clauses(self) -> list[list[Any]]:
+        ast = self.to_cnf()
+        clauses = get_clauses(ast.root)
+        if len(clauses) > 0 and not isinstance(clauses[0], list):
+            return [clauses]
+        return clauses
         
     def __str__(self) -> str:
         return str(self.root)
@@ -87,17 +94,9 @@ def convert_into_cnf(ast: AST) -> AST:
       2. Move NOTs inwards by repeatdly applying De Morgan's Law and eliminate doble negations.
       3. Distribute ORs invwards over ANDs, applying the Distribute property.
     """
-    #print(f'Before: {ast}')
     ast = eliminate_complex_operators(ast)
-    #print(f'eliminate_complex_operators: {ast}')
-    completed = False
-    while not completed:
-        (ast, completed) = move_nots_inwards(ast)
-    #print(f'move_nots_inwards: {ast}')
-    completed = False
-    while not completed:
-        (ast, completed) = distribute_ors(ast)
-    #print(f'distribute_ors: {ast}')
+    ast = move_nots_inwards(ast)
+    ast  = distribute_ors(ast)
     return ast
 
 def eliminate_implication(node: Node) -> Node:
@@ -120,37 +119,24 @@ def eliminate_exclusion(node: Node) -> Node:
     return AST.create_binary_operation(ASTOperation.OR, left, right).root 
 
 def eliminate_complex_operators(ast: AST) -> AST:
-    """Eliminate imlications, equivalences, and excludes"""
+    """Eliminate implications, equivalences, and excludes"""
     node = ast.root
-    stack = []
-    stack.append(node)
-    new_root = None
-    while stack:
-        n = stack.pop()
-        if n is not None and n.is_op():
-            new_node = None
-            if n.data == ASTOperation.REQUIRES or n.data == ASTOperation.IMPLIES:
-                new_node = eliminate_implication(n)
-            elif n.data == ASTOperation.EQUIVALENCE:
-                new_node = eliminate_implication(n)
-            elif n.data == ASTOperation.EXCLUDES:
-                new_node = eliminate_exclusion(n)
-            elif n.data == ASTOperation.NOT:
-                stack.append(n.left)
-            else:  # OR, AND nodes
-                stack.append(n.left)
-                stack.append(n.right)
-
-            if new_node is not None:
-                stack.append(new_node.left)
-                stack.append(new_node.right)
-            
-            if new_root is None:
-                if new_node is not None:
-                    new_root = new_node
-                else:
-                    new_root = n
-    return AST(new_root)
+    if node is None or not node.is_op():
+        return ast
+    new_node = None
+    if node.data == ASTOperation.REQUIRES or node.data == ASTOperation.IMPLIES:
+        new_node = eliminate_implication(node)
+    elif node.data == ASTOperation.EQUIVALENCE:
+        new_node = eliminate_equivalence(node)
+    elif node.data == ASTOperation.EXCLUDES:
+        new_node = eliminate_exclusion(node)
+    elif node.data == ASTOperation.NOT:
+        new_node = eliminate_complex_operators(AST(node.left)).root
+    else:
+        node.left = eliminate_complex_operators(AST(node.left)).root
+        node.right = eliminate_complex_operators(AST(node.right)).root
+        return AST(node)
+    return AST(new_node)
 
 def apply_demorganlaw(node: Node, operation: ASTOperation) -> Node:
     """Apply De Morgan's Law.
@@ -163,98 +149,102 @@ def apply_demorganlaw(node: Node, operation: ASTOperation) -> Node:
     new_node = AST.create_binary_operation(operation, left, right).root
     return new_node
 
-def move_nots_inwards(ast: AST) -> tuple[AST,bool]:
+def move_nots_inwards(ast: AST) -> AST:
     """Move NOTs inwards by repeatedly applying De Morgan's Law, 
     and eliminate doble negations by replacing !!P with P.
     """
     node = ast.root
-    stack = []
-    stack.append(node)
-    new_root = None
-    found_any = False
-    while stack:
-        n = stack.pop()
-        if n is not None and n.is_op():
+    if node is None or not node.is_op():
+        return ast
+    if node.data != ASTOperation.NOT:
+        node.left = move_nots_inwards(AST(node.left)).root
+        node.right = move_nots_inwards(AST(node.right)).root
+        return AST(node)
+    else:
+        if not node.left.is_op():
+            return AST(node)
+        else:
             new_node = None
-            if n.data == ASTOperation.NOT:
-                print(f'Node: {n} - {n.is_op()}, {n.data}')
-                if n.left.is_op():
-                    if n.left.data == ASTOperation.OR:
-                        new_node = apply_demorganlaw(ASTOperation.AND, n)
-                        found_any = True
-                    elif n.left.data == ASTOperation.AND:
-                        new_node = apply_demorganlaw(ASTOperation.OR, n)
-                        found_any = True
-                    elif n.left.data == ASTOperation.NOT:
-                        # Eliminate doble negation
-                        new_node = n.left.left
-                        found_any = True
-                    else:  # OR, AND nodes
-                        stack.append(n.left)
-                        stack.append(n.right)
-
-                if new_node is not None:
-                    stack.append(new_node.left)
-                    stack.append(new_node.right)
-            
-            if new_root is None:
-                if new_node is not None:
-                    new_root = new_node
-                else:
-                    new_root = n
-                    #stack.append(n.left)
-                    #stack.append(n.right)
-
-    return (AST(new_root), not found_any)
+            if node.left.data == ASTOperation.OR:
+                new_node = apply_demorganlaw(node.left, ASTOperation.AND)
+            elif node.left.data == ASTOperation.AND:
+                new_node = apply_demorganlaw(node.left, ASTOperation.OR)
+            elif node.left.data == ASTOperation.NOT:
+                # Eliminate doble negation
+                new_node = node.left.left
+            return move_nots_inwards(AST(new_node))
 
 def apply_distribution(node: Node, and_node: Node) -> Node:
     """Apply distribution property.
     
     Replace P ∨ (Q ∧ R) with (P ∨ Q) ∧ (P ∨ R).
     """
-    left = AST.create_binary_operation(ASTOperation.OR, node.left, and_node.left).root
-    right = AST.create_binary_operation(ASTOperation.OR, node.left, and_node.right).root
+    left = AST.create_binary_operation(ASTOperation.OR, node, and_node.left).root
+    right = AST.create_binary_operation(ASTOperation.OR, node, and_node.right).root
     return AST.create_binary_operation(ASTOperation.AND, left, right).root
 
-
-def distribute_ors(ast: AST) -> tuple[AST,bool]:
+def distribute_ors(ast: AST) -> AST:
     """Distribute ORs inwards over ANDs."""
     node = ast.root
-    stack = []
-    stack.append(node)
-    new_root = None
-    found_any = False
-    while stack:
-        n = stack.pop()
-        if n is not None and n.is_op():
-            new_node = None
-            if n.data == ASTOperation.OR:
-                if n.left.is_op():
-                    if n.left.data == ASTOperation.AND:
-                        new_node = apply_distribution(n, n.left)
-                        found_any = True
-                    elif n.left.data == ASTOperation.NOT and n.left.left.data == ASTOperation.AND:
-                        # Cuidado aquí, falta hacer la distributiva cuando hay negaciones.
-                        pass
-                    elif n.right.data == ASTOperation.AND:
-                        new_node = apply_distribution(n, n.right) 
-                        found_any = True
-                    else:
-                        stack.append(n.left)
-                        stack.append(n.right)
+    if node is None or not node.is_op():
+        return ast
+    if node.data != ASTOperation.OR:
+        node.left = distribute_ors(AST(node.left)).root
+        node.right = distribute_ors(AST(node.right)).root
+        return AST(node)
+    else:
+        new_node = None
+        if not node.left.is_op() and not node.right.is_op():
+            return AST(node)
 
-                    if new_node is not None:
-                        stack.append(new_node.left)
-                        stack.append(new_node.right)
-            
-            if new_root is None:
-                if new_node is not None:
-                    new_root = new_node
-                else:
-                    new_root = n
-                    #stack.append(n.left)
-                    #stack.append(n.right)
-            
-            # el problema está en que tengo que añadir al stack los n.left y n.right en caso de que la operación no sea un OR.
+        if node.left.is_op():
+            if node.left.data == ASTOperation.AND:
+                new_node = apply_distribution(node.right, node.left)
+                return distribute_ors(AST(new_node))
+        
+        if node.right.is_op():
+            if node.right.data == ASTOperation.AND:
+                new_node = apply_distribution(node.left, node.right) 
+                return distribute_ors(AST(new_node))
+        
+        if new_node is None:
+            node.left = distribute_ors(AST(node.left)).root
+            node.right = distribute_ors(AST(node.right)).root
+            return AST(node)
 
-    return (AST(new_root), not found_any)
+def get_clauses(node: Node) -> list[list[Any]]:
+    """Return the list of clauses represented by the AST root node in normal conjuntive form."""
+    if node is None or not node.is_op():
+        return []
+    if node.data == ASTOperation.AND:  # Each AND gives us two clauses
+        clauses = []
+        clauses_left = get_clauses(node.left)  # Recursive AND
+        # recursive ANDs may introduce additional clauses
+        if len(clauses_left) > 0 and isinstance(clauses_left[0], list):  
+            for c in clauses_left:
+                clauses.append(c)
+        else:
+            clauses.append(clauses_left)
+
+        clauses_right = get_clauses(node.right)  # Recursive AND
+        # recursive ANDs may introduce additional clauses
+        if len(clauses_right) > 0 and isinstance(clauses_right[0], list):  
+            for c in clauses_right:
+                clauses.append(c)
+        else:
+            clauses.append(clauses_right)
+        return clauses
+    if node.data == ASTOperation.NOT:
+        return ['-' + node.left.data]
+    if node.data == ASTOperation.OR:
+        clause = []
+        if node.left.is_op():
+            clause += get_clauses(node.left)  # recursive OR belongs to the same clause
+        else:
+            clause.append(node.left.data)
+        if node.right.is_op():
+            clause += get_clauses(node.right)  # recursive OR belongs to the same clause
+        else:
+            clause.append(node.right.data)
+        return clause
+    return []
